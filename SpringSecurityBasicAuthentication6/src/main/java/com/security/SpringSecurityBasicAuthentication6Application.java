0350ac6d34347972eece3d8391fed60f3d0f51f0
Vinayak Nair <vinayaknair1996@gmail.com>
2020-08-01 21:27:14 +0530

    Spring Boot Security Role based Authentication using @PreAuthorize annotation

---
+++ SpringSecurityBasicAuthentication6/src/main/java/com/security/SpringSecurityBasicAuthentication6Application.java
@@ -0,0 +1,14 @@
+package com.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.security.controller", "com.security.model", "com.security.config"})
+public class SpringSecurityBasicAuthentication6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityBasicAuthentication6Application.class, args);
+    }
+}
