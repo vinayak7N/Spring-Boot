c49bacb573e4358727f6607336630f88934064b4
Vinayak Nair <vinayaknair1996@gmail.com>
2020-08-04 20:26:08 +0530

    Spring Boot Security Form Based Authentication having In memory SessionId

---
+++ SpringSecurityBasicAuthentication8/src/main/java/com/security/config/ApplicationSecurityConfig.java
@@ -0,0 +1,78 @@
+package com.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import static com.security.config.ApplicationUserPermission.COURSE_WRITE;
+import static com.security.config.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // for @PreAuthorize
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                /*.antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())  //Permissions
+                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())*/ //Replaced with @PreAuthorize
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin();  //Form Based Authentication
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+
+        UserDetails lindaUser = User.builder()
+                .username("Linda")
+                .password(passwordEncoder.encode("password123"))
+                //.roles(ADMIN.name())  //ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+        UserDetails annaUser = User.builder()
+                .username("Anna")
+                .password(passwordEncoder.encode("password"))
+                //.roles(STUDENT.name())      //ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
+                .build();
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("password123"))
+                //.roles(ADMINTRAINEE.name())    // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+        return new InMemoryUserDetailsManager(
+                lindaUser, annaUser, tomUser
+        );
+    }
+}
